4abac924bab7818c72deacf05c3511c812ca3013
mascena19 <mascena19@gmail.com>
2026-01-26 01:18:41 -0300

    Projeto iniciado

---
+++ src/main/java/com/ebd/ebdonline/EbdonlineApplication.java
@@ -0,0 +1,13 @@
+package com.ebd.ebdonline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EbdonlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EbdonlineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ebd/ebdonline/EbdonlineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ebd.ebdonline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EbdonlineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
