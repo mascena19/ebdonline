f04d82f5fc01357fe1f25a4f80c6aaaf328e46c7
mascena19 <mascena19@gmail.com>
2026-01-26 01:39:04 -0300

    Enums Criados

---
+++ src/main/java/com/ebd/ebdonline/domain/enums/FaixaEtaria.java
@@ -0,0 +1,10 @@
+package com.ebd.ebdonline.domain.enums;
+
+public enum FaixaEtaria {
+    INFANTIL,
+    JUNIORES,
+    ADOLESCENTES,
+    JOVENS,
+    ADULTOS,
+    MELHOR_IDADE
+}
---
+++ src/main/java/com/ebd/ebdonline/domain/enums/Perfil.java
@@ -0,0 +1,39 @@
+package com.ebd.ebdonline.domain.enums;
+
+import java.util.Iterator;
+
+public enum Perfil {
+	
+	ADMIN(0, "ROLE_ADMIN"), SECRETADRIO(2, "ROLE_SECRETARIO");
+	
+	private Integer codigo;
+	private String descricao;
+	
+	private Perfil(Integer codigo, String descricao) {
+		this.codigo = codigo;
+		this.descricao = descricao;
+	}
+
+	public Integer getCodigo() {
+		return codigo;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+	
+	public static Perfil toEnum(Integer cod) {
+		if(cod == null) {
+			return null;
+		}
+		
+		for (Perfil x : Perfil.values()) {
+			if(cod.equals(x.getCodigo())) {
+				return x;
+			}
+		}
+		
+		throw new IllegalArgumentException("Perfil Inválido");
+		}
+
+}
